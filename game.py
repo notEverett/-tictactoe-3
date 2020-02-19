5a598609ce0eb5fc54f4d0e9f8971a3a89c0e336
Tim Adams <tim@Tims-MacBook-Air.local>
2020-02-19 18:13:28 -0500

    Testing commiting from Atom

---
+++ game.py
@@ -0,0 +1,13 @@
+import pygame
+
+black = (0,0,0)
+white = (255,255,255)
+green = (141,242,78)
+red = (219,59,59)
+blue = (64,115,227)
+
+pygame.init()
+
+size = [530,530]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('(tictactoe)^3')
